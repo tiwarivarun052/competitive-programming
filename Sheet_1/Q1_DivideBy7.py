48fff54d6757f67330b808c5b0dfa4684412481c
tiwarivarun052 <vishaltiwari6398810969@gmail.com>
2025-08-23 16:18:45 +0530

    first commit

--- Sheet_1/Q1_DivideBy7.py
+++ Sheet_1/Q1_DivideBy7.py
@@ -6,3 +6,7 @@ if num % 7 == 0:
 	print(f"{num} is divisible by 7.")
 else:
 	print(f"{num} is not divisible by 7.")
+
+
+
+
---
+++ extra/pyramid_trangle.py
@@ -0,0 +1,6 @@
+n = int(input("Enter N: "))
+
+for i in range(n,0,-1):
+    for j in range(i):
+        print("*", end="")
+    print()
