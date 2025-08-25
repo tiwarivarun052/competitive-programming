5c4d8c67f1c02427fbdbe950be95fe56c97f56bf
tiwarivarun052 <vishaltiwari6398810969@gmail.com>
2025-08-25 14:28:06 +0530

    commit again

--- extra/printPattern.py
+++ extra/printPattern.py
@@ -20,8 +20,26 @@
 #     print()
 
 
+# n = int(input("Enter: "))
+# for i in range(n,0,-1):
+#     for j in range(i):
+#         print("*", end="")
+#     print()
+
+# n = int(input("Enter: "))
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         if(j%2==0):
+#             print("*", end="")
+#         else:
+#             print(j, end="")
+#     print()
+
 n = int(input("Enter: "))
-for i in range(n,0,-1):
-    for j in range(i):
-        print("*", end="")
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if(j==1 or j==n):
+            print("*", end="")
+        else:
+            print("_", end="")
     print()
