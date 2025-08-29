418c6fbae7817dfacc1d5e7c8a47675233d9a8de
tiwarivarun052 <vishaltiwari6398810969@gmail.com>
2025-08-29 09:03:25 +0530

    commited

--- extra/printPattern.py
+++ extra/printPattern.py
@@ -88,15 +88,15 @@
 #         print("*", end="")
 #     print()
 
-n = int(input("Enter:"))
-for i in range(1, n+1):
-    for j in range(i):
-        print("*", end="")
-    for j in range(2*(n-i)):
-        print(" ", end="")
-    for j in range(1,i+1):
-        print("*", end="")
-    print()
+# n = int(input("Enter:"))
+# for i in range(1, n+1):
+#     for j in range(i):
+#         print("*", end="")
+#     for j in range(2*(n-i)):
+#         print(" ", end="")
+#     for j in range(1,i+1):
+#         print("*", end="")
+#     print()
 
 # n = int(input("Enter:"))
 # for i in range(1, n+1):
